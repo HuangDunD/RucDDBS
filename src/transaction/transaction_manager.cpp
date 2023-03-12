28d3e3103540cf261341909eff0598d1df9b3f69
hcy <1106412975@qq.com>
2023-03-12 22:36:37 +0800

    add transaction manager header

---
+++ src/transaction/transaction_manager.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Transaction.h"
+#include "Lock_manager.h"
+
+enum class ConcurrencyMode { TWO_PHASE_LOCKING = 0, BASIC_TO };
+
+class TransactionManager
+{
+private:
+    /* data */
+public:
+    TransactionManager(){};
+    ~TransactionManager(){};
+
+    explicit TransactionManager(Lock_manager *lock_manager, SmManager *sm_manager,
+                                ConcurrencyMode concurrency_mode = ConcurrencyMode::TWO_PHASE_LOCKING) {
+        
+    }
+};
