a60c5dbf8424901037a7908696920df3029c20ed
hcy <1106412975@qq.com>
2023-03-16 19:35:16 +0800

    add transaction manager header

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -1,20 +1,52 @@
-#pragma once
+#include "transaction_manager.h"
 
-#include "Transaction.h"
-#include "Lock_manager.h"
+uint64_t TransactionManager::getTimestampFromServer(){
+    brpc::Channel channel;
+    brpc::ChannelOptions options;
+    options.timeout_ms = 100;
+    options.max_retry = 3;
 
-enum class ConcurrencyMode { TWO_PHASE_LOCKING = 0, BASIC_TO };
-
-class TransactionManager
-{
-private:
-    /* data */
-public:
-    TransactionManager(){};
-    ~TransactionManager(){};
-
-    explicit TransactionManager(Lock_manager *lock_manager, 
-            ConcurrencyMode concurrency_mode = ConcurrencyMode::TWO_PHASE_LOCKING) {
-        
+    if (channel.Init(server, &options) != 0) {
+        LOG(ERROR) << "Fail to initialize channel";
+        return -1;
     }
-};
+    meta_service::MetaService_Stub stub(&channel);
+    brpc::Controller cntl;
+    meta_service::getTimeStampRequest request;
+    meta_service::getTimeStampResponse response;
+    stub.GetTimeStamp(&cntl, &request, &response, NULL);
+
+    if(cntl.Failed()) {
+        LOG(WARNING) << cntl.ErrorText();
+    }
+    return response.timestamp();
+}
+
+Transaction* TransactionManager::Begin(Transaction* txn, LogManager *log_manager){
+    // Todo:
+    // 1. 判断传入事务参数是否为空指针
+    // 2. 如果为空指针，创建新事务
+    // 3. 把开始事务加入到全局事务表中
+    // 4. 返回当前事务指针
+    if (txn == nullptr) {
+        txn = new Transaction(getTimestampFromServer(), IsolationLevel::SERIALIZABLE));
+    }
+}
+
+void TransactionManager::Abort(Transaction * txn, LogManager *log_manager){
+
+}
+
+void TransactionManager::Commit(Transaction * txn, LogManager *log_manager){
+
+}
+
+/** Prevents all transactions from performing operations, used for checkpointing. */
+void TransactionManager::BlockAllTransactions(){
+
+}
+
+/** Resumes all transactions, used for checkpointing. */
+void TransactionManager::ResumeTransactions(){
+
+}
---
+++ src/transaction/transaction_manager.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Transaction.h"
+#include "Lock_manager.h"
+#include "meta_service.pb.h"
+
+#include <shared_mutex>
+#include <brpc/channel.h>
+#include <butil/logging.h>
+
+#define server "[::1]:8001"
+
+enum class ConcurrencyMode { TWO_PHASE_LOCKING = 0, BASIC_TO };
+
+class LogManager;
+
+class TransactionManager
+{
+private:
+    Lock_manager *lock_manager_;
+    LogManager *log_manager_;
+    ConcurrencyMode concurrency_mode_;
+
+public:
+    TransactionManager(){};
+    ~TransactionManager() = default;
+
+    explicit TransactionManager(Lock_manager *lock_manager, LogManager *log_manager = nullptr,
+            ConcurrencyMode concurrency_mode = ConcurrencyMode::TWO_PHASE_LOCKING) {
+        lock_manager_ = lock_manager;
+        log_manager_ = log_manager;
+        concurrency_mode_ = concurrency_mode;
+    }
+    ConcurrencyMode getConcurrencyMode() { return concurrency_mode_; }
+    void SetConcurrencyMode(ConcurrencyMode concurrency_mode) { concurrency_mode_ = concurrency_mode; }
+    Lock_manager *getLockManager() { return lock_manager_; }
+
+    /* The transaction map is a local list of all the running transactions in the local node. */
+    static std::unordered_map<txn_id_t, Transaction *> txn_map;
+    static std::shared_mutex txn_map_mutex;
+
+    static Transaction *getTransaction(txn_id_t txn_id) {
+        std::shared_lock<std::shared_mutex> l(txn_map_mutex);
+        if(TransactionManager::txn_map.find(txn_id) == TransactionManager::txn_map.end()){
+            return nullptr;
+        }
+        Transaction *txn = txn_map[txn_id];
+        assert(txn != nullptr);
+        return txn;
+    }
+
+    uint64_t getTimestampFromServer();
+    
+    Transaction* Begin(Transaction* txn, LogManager *log_manager);
+
+    void Abort(Transaction * txn, LogManager *log_manager);
+
+    void Commit(Transaction * txn, LogManager *log_manager);
+
+    /** Prevents all transactions from performing operations, used for checkpointing. */
+    void BlockAllTransactions();
+
+    /** Resumes all transactions, used for checkpointing. */
+    void ResumeTransactions();
+};
