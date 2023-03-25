a4cdc28a71c376442f49b915fef9e9570e22ad6b
hcy <1106412975@qq.com>
2023-03-25 21:36:28 +0800

    add logstorage.h

--- include/storage/LogStorage.h
+++ include/storage/LogStorage.h
@@ -1,46 +1,87 @@
+#pragma once
 #include <string>
-// log file
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <sys/stat.h>
+
 static const std::string LOG_FILE_NAME = "db.log";
 
 class LogStorage
 {
 private:
-    int log_fd_ = -1;              // log file
-
+    // file name for log file
+    std::string log_name_;
+    // file stream for log file
+    std::fstream log_file_;
 public:
-    LogStorage(/* args */);
+    LogStorage(std::string db_name_){
+
+        auto n = db_name_.rfind('.');
+        log_name_ = db_name_.substr(0, n) + ".log";
+
+        // open log file stream
+        log_file_.open(log_name_, std::ios::binary | std::ios::in | std::ios::app | std::ios::out);
+        if (!log_file_.is_open()) {
+            // log is not opended, which means the file doesn't exist
+            // then we create it
+            log_file_.clear();
+            // std::ios::in will fail us when the file is not exist 
+            log_file_.open(log_name_, std::ios::binary | std::ios::trunc | std::ios::app | std::ios::out);
+            log_file_.close();
+            // reopen it with original mode
+            log_file_.open(log_name_, std::ios::binary | std::ios::in | std::ios::app | std::ios::out);
+            if (!log_file_.is_open()) {
+                std::cerr << ("failed to open log file, filename: %s", log_name_.c_str());
+            }
+        }
+    };
     ~LogStorage();
 
-    bool ReadLog(char *log_data, int size, int offset, int prev_log_end) {
-        // read log file from the previous end
-        if (log_fd_ == -1) {
-            log_fd_ = open_file(LOG_FILE_NAME);
-        }
-        offset += prev_log_end;
-        int file_size = GetFileSize(LOG_FILE_NAME);
-        if (offset >= file_size) {
+    int GetFileSize(const std::string &filename) {
+        struct stat stat_buf;
+        int rc = stat(filename.c_str(), &stat_buf);
+        return rc == 0 ? static_cast<int> (stat_buf.st_size) : -1;
+    }
+
+    bool ReadLog(char *log_data, int size, int offset) {
+
+        // this is stupid. we should cache the log size then read it till end
+        // since log file won't change while performing recovery protocol.
+        if (offset >= GetFileSize(log_name_)) {
             return false;
         }
 
-        size = std::min(size, file_size - offset);
-        lseek(log_fd_, offset, SEEK_SET);
-        ssize_t bytes_read = read(log_fd_, log_data, size);
-        if (bytes_read != size) {
-            throw UnixError();
+        log_file_.seekp(offset);
+        log_file_.read(log_data, size);
+
+        if (log_file_.bad()) {
+            std::cerr << ("I/O error while reading log");
+            return false;
         }
+
+        int read_count = log_file_.gcount();
+        if (read_count < size) {
+            log_file_.clear();
+            // pad with zero
+            memset(log_data + read_count, 0, size - read_count);
+        }
+
         return true;
     }
 
     void WriteLog(char *log_data, int size) {
-        if (log_fd_ == -1) {
-            log_fd_ = open_file(LOG_FILE_NAME);
+        if (size == 0) {
+            return;
         }
-
-        // write from the file_end
-        lseek(log_fd_, 0, SEEK_END);
-        ssize_t bytes_write = write(log_fd_, log_data, size);
-        if (bytes_write != size) {
-            throw UnixError();
+        // append the log
+        log_file_.write(log_data, size);
+        // check for IO-error
+        if (log_file_.bad()) {
+            std::cerr << "I/O error while writing log";
+            return;
         }
+        // flush to disk
+        log_file_.flush();
     }
 };
