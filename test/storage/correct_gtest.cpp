765e320a4657ab151194f24bde3693962c506a57
YangHarvey <2998647748@qq.com>
2023-03-26 06:09:43 -0700

    add test for storage

--- test/storage/correct_gtest.cpp
+++ test/storage/correct_gtest.cpp
@@ -5,14 +5,56 @@
 
 KVStore storage = KVStore("./data");
 
-TEST(StorageTest, single_value) { 
-    // const std::string empty_string = "";
-    EXPECT_EQ("", storage.get(1));
-    storage.put(1, "ysm_data");
-    EXPECT_EQ("ysm_data", storage.get(1));
-    EXPECT_EQ(true, storage.del(1));
-    EXPECT_EQ("", storage.get(1));
-	EXPECT_EQ(false, storage.del(1));
+TEST(StorageTest, single_test) { 
+  // const std::string empty_string = "";
+  EXPECT_EQ("", storage.get(1));
+  storage.put(1, "ysm_data");
+  EXPECT_EQ("ysm_data", storage.get(1));
+  EXPECT_EQ(true, storage.del(1));
+  EXPECT_EQ("", storage.get(1));
+  EXPECT_EQ(false, storage.del(1));
+}
+
+TEST(StorageTest, small_test) {
+  const uint64_t small = 1024;
+  // put
+  for(uint64_t i = 1; i <= small; i++){
+    storage.put(i, std::string(i, 's'));
+    EXPECT_EQ(std::string(i, 's'), storage.get(i));
+  }
+  // get
+  for(uint64_t i = 1; i <= small; i++) {
+    EXPECT_EQ(std::string(i, 's'), storage.get(i));
+  }
+  //delete
+  for(uint64_t i = 1; i <= small; i++) {
+    EXPECT_EQ(true, storage.del(i));
+  }
+  // get
+  for(uint64_t i = 1; i <= small; i++) {
+    EXPECT_EQ("", storage.get(i));
+  }
+}
+
+TEST(StorageTest, large_test) {
+  const uint64_t large = 1024 * 64;
+  // put
+  for(uint64_t i = 1; i <= large; i++){
+    storage.put(i, std::string(i, 's'));
+    EXPECT_EQ(std::string(i, 's'), storage.get(i));
+  }
+  // get
+  for(uint64_t i = 1; i <= large; i++) {
+    EXPECT_EQ(std::string(i, 's'), storage.get(i));
+  }
+  //delete
+  for(uint64_t i = 1; i <= large; i++) {
+    EXPECT_EQ(true, storage.del(i));
+  }
+  // get
+  for(uint64_t i = 1; i <= large; i++) {
+    EXPECT_EQ("", storage.get(i));
+  }
 }
 
 int main(int argc, char **argv) {
