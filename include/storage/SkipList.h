cf4a81917e0c3cd6d0617117c85a4d4a62f6822a
YangHarvey <2998647748@qq.com>
2023-05-05 22:08:28 +0800

    skiplist_test

--- include/storage/SkipList.h
+++ include/storage/SkipList.h
@@ -13,20 +13,16 @@ public:
     explicit SkipList();
     ~SkipList();
 
-    // insert (key, value) pair into the skiplist
-    // if there already exists a key that equals to the given key,
-    // then update the value
+    // 向跳表中插入键值对(key, value)。如果跳表中已经存在与给定key相同key的键值对，那么更新该key所对应的value
     void put(const std::string &key, const std::string &value);
 
-    // get value for the given key, if the key value pair doesn't exist
-    // return <false, "">
+    // 获得给定key所对应的value。如果跳表中存在对应的键值对，那么返回(true, value)，否则返回(false, "")
     std::pair<bool, std::string> get(const std::string &key) const;
 
-    // delete the (key, value) pair in skiplist, if the (key, value) pair
-    // doesn't exist, return false
+    // 删除跳表中的指定键值对(key, value)。如果删除成功返回true，否则返回false，如果指定的key不存在，返回false
     bool del(const std::string &key);
 
-    // return true if the key in skiplist
+    // 返回跳表是否存在指定的key。如果指定的key存在，返回true，否则返回false
     bool contains(const std::string &key) const;
 
     // Iterator newIterator() const;
@@ -63,7 +59,7 @@ private:
     // create head_ and tail_ node in skiplist
     void init();
 
-    // enlarge the height of head_ and tail_ in skiplist
+    // 当新节点的高度超过当前的最高节点高度（即head和tail节点的高度）时，需要扩充跳表的最高节点（即head和tail节点）的高度
     void enlargeHeight(size_t height);
 
     // return a random height that is not greater than max_height_
@@ -72,14 +68,13 @@ private:
     // return the current max height in skiplist
     size_t getMaxHeight() const;
 
-    // find the first node which is greater or euqal to the given key
-    // and if prevs is not nullptr, node's prevs nodes will be store in prevs
+    // 查找大于或者等于指定key的第一个节点，如果没有节点大于指定的key，那么返回tail节点。如果prevs不等于nullptr，那么prevs会保存该节点的所有前缀节点。
     Node *findGreatorOrEqual(const std::string &key, Node **prevs) const;
     
-    //
+    // 查找最后一个小于指定key的节点，如果没有节点小于指定的key，那么返回head节点
     Node *findLessThan(const std::string &key) const;
 
-    //
+    // 查找除`tail`结点之外的最后一个节点。
     Node *findLast() const;
 };
 
--- src/storage/SkipList.cpp
+++ src/storage/SkipList.cpp
@@ -28,92 +28,34 @@ void SkipList::clear()
     init();
 }
 
-// get the value of the key, if not found, return empty string
+// 功能：获得给定key所对应的value。
+// 返回值：如果跳表中存在对应的键值对，那么返回(true, value)，否则返回(false, "")
 std::pair<bool, std::string> SkipList::get(const std::string &key) const
 {
-    Node *node = findGreatorOrEqual(key, nullptr);
-    if (node != tail_ && node->key_ == key)
-    {
-        return std::make_pair(true, node->value_);
-    }
-    else
-    {
-        return std::make_pair(false, "");
-    }
-    // return node != head_ && node->key_ == key ? node->value_ : "";
+
 }
 
-// insert a new (key, value) pair into skiplist, if the key is already in
-// the skiplist, then update the value, if not, then create a new node using
+// 功能：向跳表中插入键值对(key, value)。如果跳表中已经存在与给定key相同key的键值对，那么更新该key所对应的value
+// 返回值：void
 void SkipList::put(const std::string &key, const std::string &value)
 {
-    // find key
-    Node *prevs[max_height_];
-    Node *x = findGreatorOrEqual(key, prevs);
-    // if equal, update the value
-    if (x != tail_ && x->key_ == key)
-    {
-        num_bytes_ = num_bytes_ - x->value_.size() + value.size();
-        x->value_ = value;
-        return;
-    }
-    size_t height = random_height();
-    if (height > getMaxHeight())
-    {
-        for (size_t i = getMaxHeight(); i < height; i++)
-        {
-            prevs[i] = head_;
-        }
-        enlargeHeight(height);
-    }
-    // update the pointers
-    Node *node = new Node(key, value, height);
-    for (size_t i = 0; i < height; i++)
-    {
-        node->nexts_[i] = prevs[i]->nexts_[i];
-        prevs[i]->nexts_[i] = node;
-    }
-    num_entries_++;
-    num_bytes_ = num_bytes_ + key.size() + value.size();
-    return;
+
 }
 
-// delete (key, value) pair
+// 功能：删除跳表中的指定键值对(key, value)。
+// 返回值：如果删除成功返回true，否则返回false，如果指定的key不存在，返回false
 bool SkipList::del(const std::string &key)
 {
-    // find the node
-    Node *prevs[max_height_];
-    Node *node = findGreatorOrEqual(key, prevs);
-    if (node == tail_ || node->key_ != key)
-    {
-        return false;
-    }
-    size_t height = node->height_;
-    for (size_t i = 0; i < height; i++)
-    {
-        prevs[i]->nexts_[i] = node->nexts_[i];
-    }
-    num_entries_--;
-    num_bytes_ = num_bytes_ - node->key_.size() - node->value_.size();
-    delete node;
-    return true;
+
 }
 
+// 功能：返回跳表是否存在指定的key。
+// 返回值：如果指定的key存在，返回true，否则返回false
 bool SkipList::contains(const std::string &key) const
 {
-    Node *node = findGreatorOrEqual(key, nullptr);
-    return node != tail_ && node->key_ == key;
+    
 }
 
-
-
-// uint64_t SkipList::space() const {
-//     return (num_entries_ * 2 + num_bytes_ / Option::BLOCK_SPACE * 2 + 6) * sizeof(uint64_t) + num_bytes_;
-// }
-
-// SkipList::Iterator SkipList::newIterator() const {
-//     return Iterator(this);
-// }
 
 void SkipList::init()
 {
@@ -125,6 +67,7 @@ void SkipList::init()
     num_entries_ = 0;
 }
 
+// 返回一个随机的高度值，小于max_height_
 size_t SkipList::random_height()
 {
     size_t height = 1;
@@ -138,96 +81,29 @@ size_t SkipList::getMaxHeight() const
     return head_->height_;
 }
 
+// 功能：当新节点的高度超过当前的最高节点高度（即head和tail节点的高度）时，需要扩充跳表的最高节点（即head和tail节点）的高度
+// 返回值：void
 void SkipList::enlargeHeight(size_t height)
 {
-    size_t oldHeight = head_->height_;
-    head_->height_ = height;
-    tail_->height_ = height;
-    Node **oldHeadNexts = head_->nexts_;
-    Node **oldTailNexts = tail_->nexts_;
-    head_->nexts_ = new Node *[height];
-    tail_->nexts_ = new Node *[height];
-    for (size_t i = 0; i < height; ++i)
-        tail_->nexts_[i] = nullptr;
-    for (size_t i = 0; i < oldHeight; ++i)
-    {
-        head_->nexts_[i] = oldHeadNexts[i];
-    }
-    for (size_t i = oldHeight; i < height; ++i)
-    {
-        head_->nexts_[i] = tail_;
-    }
-    delete[] oldHeadNexts;
-    delete[] oldTailNexts;
+
 }
 
-// TODO findGreatorOrEqual
-// return the first node that greater or equal to the given key
-// if no such value, return tail_
+// 功能：查找大于或者等于指定key的第一个节点。
+// 返回值：如果没有节点大于指定的key，那么返回tail节点。如果prevs不等于nullptr，那么prevs会保存该节点的所有前缀节点。
 SkipList::Node *SkipList::findGreatorOrEqual(const std::string &key, Node **prevs) const
 {
-    Node *x = head_;
-    int height = getMaxHeight() - 1;
-    while (true)
-    {
-        Node *next = x->nexts_[height];
-        if (next != tail_ && next->key_ < key)
-        {
-            // search in this level
-            x = next;
-        }
-        else
-        {
-            if (prevs != nullptr)
-            {
-                prevs[height] = x;
-            }
-            if (height == 0)
-            {
-                return next;
-            }
-            else
-            {
-                height--;
-            }
-        }
-    }
+    
 }
 
-// TODO find less Than key
+// 功能：查找最后一个小于指定key的节点
+// 返回值：如果没有节点小于指定的key，那么返回head节点
 SkipList::Node* SkipList::findLessThan(const std::string &key) const {
-    Node *node = head_;
-    size_t level = getMaxHeight() - 1;
-    while(true) {
-        Node *next = node->nexts_[level];
-        if(next != tail_ && next->key_ < key){
-            node = next;
-        }else {
-            if(level == 0){
-                return node;
-            }else{
-                level--;
-            }
-        }
-    }
+    
 }
 
-//
+// 功能：查找除tail结点之外的最后一个节点。
 SkipList::Node *SkipList::findLast() const {
-    Node *node = head_;
-    size_t level = getMaxHeight() - 1;
-    while(true) {
-        Node *next = node->nexts_[level];
-        if(next != tail_) {
-            node = next;
-        }else {
-            if(level == 0) {
-                return node;
-            }else{
-                level--;
-            }
-        }
-    }
+    
 }
 
 // Node
