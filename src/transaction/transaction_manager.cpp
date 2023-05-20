e210faa6d198f34f851449d659c90ce45e0d568d
hcy <1106412975@qq.com>
2023-05-20 10:34:28 +0800

    ********

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -135,6 +135,8 @@ Transaction* TransactionManager::Begin(Transaction*& txn, txn_id_t txn_id, Isola
     }
 
     std::unique_lock<std::shared_mutex> l(txn_map_mutex);
+    if(txn_map.find(txn->get_txn_id()) != txn_map.end()) 
+        std::cout <<"******************************" << std::endl;
     txn_map[txn->get_txn_id()] = txn;
     return txn;
 }
