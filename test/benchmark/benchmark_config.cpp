d7e8add346fa29e82b1563015ea925a83812a70d
hcy <1106412975@qq.com>
2023-05-21 11:14:57 +0800

    1

--- test/benchmark/benchmark_config.cpp
+++ test/benchmark/benchmark_config.cpp
@@ -7,4 +7,4 @@ DEFINE_string(DIR, "./data2", "data dir");
 DEFINE_int32(OP_MAX_NUM, 30, "max ops num in a txn");
 
 // std::vector<IP_Port> NodeSet = {{"127.0.0.1",8011},{"127.0.0.1",8012},{"127.0.0.1",8013}};
-std::vector<IP_Port> NodeSet = {{"172.27.163.66",8011},{"172.24.162.221",8012},{"172.22.168.27",8013}};
+std::vector<IP_Port> NodeSet = {{"172.22.168.27",8011},{"172.24.162.221",8012},{"172.27.163.66",8013}};
